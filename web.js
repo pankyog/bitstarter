61fdf716f67b83d52789a3a34ab887b2059ae048
Pankaj Negi <pankyog@gmail.com>
2013-07-12 05:00:09 +0000

    update

--- web.js
+++ web.js
@@ -2,7 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var buf = new Buffer(256);
+
+
 var fs = require('fs'); 
+
+
+fs.readFileSync('index.html', buffer);
+
 
 
 app.get('/', function(request, response) {
